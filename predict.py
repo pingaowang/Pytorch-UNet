91ffdd3796db3a04be5819a09f05c67f3634ded0
pingaowang <pingaowang2015@gmail.com>
2019-06-11 12:48:37 -0400

    debug: predict pipeline 2

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ import torch.nn.functional as F
 from PIL import Image
 
 from unet import UNet
-from utils import resize_and_crop, normalize, split_img_into_squares, hwc_to_chw, merge_masks, dense_crf
+from utils import resize_and_crop, normalize, split_img_into_squares, hwc_to_chw, merge_masks, dense_crf, int01_3darr_save2png, listdir_check
 from utils import plot_img_and_mask
 
 from torchvision import transforms
@@ -27,24 +27,29 @@ def predict_img(net,
     img = resize_and_crop(full_img, scale=scale_factor)
     img = normalize(img)
 
-    left_square, right_square = split_img_into_squares(img)
-
-    left_square = hwc_to_chw(left_square)
-    right_square = hwc_to_chw(right_square)
-
-    X_left = torch.from_numpy(left_square).unsqueeze(0)
-    X_right = torch.from_numpy(right_square).unsqueeze(0)
+    # left_square, right_square = split_img_into_squares(img)
+    #
+    # left_square = hwc_to_chw(left_square)
+    # right_square = hwc_to_chw(right_square)
+    square = hwc_to_chw(img)
+    #
+    # X_left = torch.from_numpy(left_square).unsqueeze(0)
+    # X_right = torch.from_numpy(right_square).unsqueeze(0)
+    img = torch.from_numpy(square).unsqueeze(0)
     
     if use_gpu:
-        X_left = X_left.cuda()
-        X_right = X_right.cuda()
+        # X_left = X_left.cuda()
+        # X_right = X_right.cuda()
+        img = img.cuda()
 
     with torch.no_grad():
-        output_left = net(X_left)
-        output_right = net(X_right)
+        # output_left = net(X_left)
+        # output_right = net(X_right)
+        output = net(img)
 
-        left_probs = output_left.squeeze(0)
-        right_probs = output_right.squeeze(0)
+        # left_probs = output_left.squeeze(0)
+        # right_probs = output_right.squeeze(0)
+        probs = output.squeeze(0)
 
         tf = transforms.Compose(
             [
@@ -54,18 +59,22 @@ def predict_img(net,
             ]
         )
         
-        left_probs = tf(left_probs.cpu())
-        right_probs = tf(right_probs.cpu())
+        # left_probs = tf(left_probs.cpu())
+        # right_probs = tf(right_probs.cpu())
+        probs = tf(probs.cpu())
 
-        left_mask_np = left_probs.squeeze().cpu().numpy()
-        right_mask_np = right_probs.squeeze().cpu().numpy()
+        # left_mask_np = left_probs.squeeze().cpu().numpy()
+        # right_mask_np = right_probs.squeeze().cpu().numpy()
+        mask_np = probs.squeeze().cpu().numpy()
 
-    full_mask = merge_masks(left_mask_np, right_mask_np, img_width)
+    # full_mask = merge_masks(left_mask_np, right_mask_np, img_width)
 
     if use_dense_crf:
-        full_mask = dense_crf(np.array(full_img).astype(np.uint8), full_mask)
+        # full_mask = dense_crf(np.array(full_img).astype(np.uint8), full_mask)
+        full_mask = dense_crf(np.array(full_img).astype(np.uint8), mask_np)
 
-    return full_mask > out_threshold
+    # return full_mask > out_threshold
+    return mask_np > out_threshold
 
 
 
@@ -75,11 +84,11 @@ def get_args():
                         metavar='FILE',
                         help="Specify the file in which is stored the model"
                              " (default : 'MODEL.pth')")
-    parser.add_argument('--input', '-i', metavar='INPUT', nargs='+',
-                        help='filenames of input images', required=True)
+    parser.add_argument('--input', '-i', metavar='INPUT',
+                        help='dir path of input images', required=True)
 
-    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+',
-                        help='filenames of ouput images')
+    parser.add_argument('--output', '-o', metavar='OUTPUT',
+                        help='dir path of ouput images')
     parser.add_argument('--cpu', '-c', action='store_true',
                         help="Do not use the cuda version of the net",
                         default=False)
@@ -97,35 +106,49 @@ def get_args():
                         default=0.5)
     parser.add_argument('--scale', '-s', type=float,
                         help="Scale factor for the input images",
-                        default=0.5)
+                        default=1)
 
     return parser.parse_args()
 
-def get_output_filenames(args):
-    in_files = args.input
-    out_files = []
+# def get_output_filenames(args):
+#     in_files = args.input
+#     out_files = []
+#
+#     if not args.output:
+#         for f in in_files:
+#             pathsplit = os.path.splitext(f)
+#             out_files.append("{}_OUT{}".format(pathsplit[0], pathsplit[1]))
+#     elif len(in_files) != len(args.output):
+#         print("Error : Input files and output files are not of the same length")
+#         raise SystemExit()
+#     else:
+#         out_files = args.output
+#
+#     return out_files
 
-    if not args.output:
-        for f in in_files:
-            pathsplit = os.path.splitext(f)
-            out_files.append("{}_OUT{}".format(pathsplit[0], pathsplit[1]))
-    elif len(in_files) != len(args.output):
-        print("Error : Input files and output files are not of the same length")
-        raise SystemExit()
-    else:
-        out_files = args.output
-
-    return out_files
 
 def mask_to_image(mask):
     return Image.fromarray((mask * 255).astype(np.uint8))
 
+
+def data_loader(in_dir, out_dir):
+    # load in_files
+    list_i_name = os.listdir(in_dir)
+    listdir_check(list_i_name)
+    list_i = []
+    list_o = []
+    for i_name in list_i_name:
+        list_o.append(os.path.join(out_dir, 'pred_' + i_name))
+        list_i.append(os.path.join(in_dir, i_name))
+
+    return list_i, list_o
+
+
 if __name__ == "__main__":
     args = get_args()
-    in_files = args.input
-    out_files = get_output_filenames(args)
+    in_files, out_files = data_loader(args.input, args.output)
 
-    net = UNet(n_channels=1, n_classes=1)
+    net = UNet(n_channels=1, n_classes=2)
 
     print("Loading model {}".format(args.model))
 
@@ -164,7 +187,9 @@ if __name__ == "__main__":
 
         if not args.no_save:
             out_fn = out_files[i]
-            result = mask_to_image(mask)
-            result.save(out_files[i])
+            # result = mask_to_image(mask)
+            # result.save(out_files[i])
+            mask = mask.astype(int)
+            int01_3darr_save2png(mask, out_fn)
 
             print("Mask saved to {}".format(out_files[i]))
