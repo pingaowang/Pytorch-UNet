8bd0b63ecd6c30acb5c87b0db0b1fff7ad273a6e
pingaowang <pingaowang2015@gmail.com>
2019-12-10 18:11:25 -0500

    debug

--- train_line_v2_full_v2.sh
+++ train_line_v2_full_v2.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- codinbg: utf-8 -*-
 
-python3 train.py -l 0.0001 --nz 0.001 -d 0.982 -e 300 -b 4 -g --task-type line --data data/dataset_line_v2 --log log_line_v2_full_v3_init
+python3 train.py -l 0.0001 --nz 0.001 -d 0.982 -e 300 -b 4 -g --task-type line --data data/dataset_line_v2_b4 --log log_line_v2_full_v3_init
