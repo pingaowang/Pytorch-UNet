95a8c492b5f914501a012fd4fb9c05fe062ba515
pingaowang <pingaowang2015@gmail.com>
2019-12-10 10:43:07 -0500

    debug

--- train_line_v2_small_nz.sh
+++ train_line_v2_small_nz.sh
@@ -2,4 +2,4 @@
 # -*- codinbg: utf-8 -*-
 
 ## line_v2_small dataset, + loss_non_zero
-python train.py -l 0.0001 -d 1 -e 40 -b 2 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz
+python3 train.py -l 0.0001 -d 1 -e 40 -b 2 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz
