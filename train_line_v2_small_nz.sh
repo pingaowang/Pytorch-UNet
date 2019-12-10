8c1b8c4bf1144ab4a7b4452b46d5513623cca753
pingaowang <pingaowang2015@gmail.com>
2019-12-10 12:10:42 -0500

    debu

--- train_line_v2_small_nz.sh
+++ train_line_v2_small_nz.sh
@@ -2,13 +2,13 @@
 # -*- codinbg: utf-8 -*-
 
 ## line_v2_small dataset, + loss_non_zero = 1
-python3 train.py -l 0.0001 --nz 1 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz
+python3 train.py -l 0.0001 --nz 1 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz_1
 
 ## line_v2_small dataset, + loss_non_zero = 0.5
-python3 train.py -l 0.0001 --nz 0.5 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz
+python3 train.py -l 0.0001 --nz 0.5 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz_0-5
 
 ## line_v2_small dataset, + loss_non_zero = 0.1
-python3 train.py -l 0.0001 --nz 0.1 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz
+python3 train.py -l 0.0001 --nz 0.1 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz_0-1
 
 ## line_v2_small dataset, + loss_non_zero = 0.01
-python3 train.py -l 0.0001 --nz 0.01 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz
+python3 train.py -l 0.0001 --nz 0.01 -d 1 -e 60 -b 4 -g --task-type line --data data/dataset_line_v2_small --log log_line_v2_10b_nz0-01
