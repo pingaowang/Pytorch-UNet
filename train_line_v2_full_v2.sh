17cc2ae3c40c7b84d8a8a268461afc273faed7e8
pingaowang <pingaowang2015@gmail.com>
2019-12-10 21:31:52 -0500

    debug

--- train_line_v2_full_v2.sh
+++ train_line_v2_full_v2.sh
@@ -2,5 +2,5 @@
 # -*- codinbg: utf-8 -*-
 
 # python3 train.py -l 0.0001 --nz 0.001 -d 0.982 -e 300 -b 4 -g --task-type line --data data/dataset_line_v2_b4 --log log_line_v2_full_v3_init
-python3 train.py -l 0.00003 --nz 0.001 -d 0.982 -e 300 -b 4 -g --task-type line --data data/dataset_line_v2_b4 --log log_line_v2_full_v3_resume_1 -c log_line_v2_full_v2_init/checkpoints/CP60.pth
+python3 train.py -l 0.00003 --nz 0.001 -d 0.982 -e 300 -b 4 -g --task-type line --data data/dataset_line_v2_b4 --log log_line_v2_full_v3_resume_1 -c log_line_v2_full_v3_init/checkpoints/CP60.pth
 
