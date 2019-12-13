31d9c708d59a0bd68607b8d17a5c35d21361041d
pingaowang <pingaowang2015@gmail.com>
2019-12-13 10:24:01 -0500

    exp id_1: resume 1

--- train_line_v3_val4_id-01.sh
+++ train_line_v3_val4_id-01.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 # -*- codinbg: utf-8 -*-
 
-python3 train.py -l 0.0001 --resize 500 --nz 0.001 -d 0.995 -e 6000 -b 4 -g --task-type line --data data/dataset_line_v3_val4 --log log_line_v3_val4_id_01
-
+#python3 train.py -l 0.0001 --resize 500 --nz 0.001 -d 0.995 -e 130 -b 4 -g --task-type line --data data/dataset_line_v3_val4 --log log_line_v3_val4_id_01
+python3 train.py -l 0.00001 --resize 500 --nz 0 -d 0.995 -e 200 -b 4 -g --task-type line --data data/dataset_line_v3_val4 --log log_line_v3_val4_id_01_resume_1 -c log_line_v3_val4_id_01/checkpoints/CP130.pth
