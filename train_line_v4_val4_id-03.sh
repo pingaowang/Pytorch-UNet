a4b7336fa0cc9f3ac7853e28304237405a7d435b
pingaowang <pingaowang2015@gmail.com>
2019-12-22 05:44:03 +0800

    debug

--- train_line_v4_val4_id-03.sh
+++ train_line_v4_val4_id-03.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- codinbg: utf-8 -*-
 
-python3 train.py -l 0.0001 --resize 200 --nz 0 -d 0.9 -e 100 -b 16 -g --task-type line --data data/dataset_line_v4_val4 --log log_line_v4_val4_id_03
+python3 train.py -l 0.0001 --resize 200 --nz 0 -d 0.9 -e 100 -b 24 -g --task-type line --data data/dataset_line_v4_val4 --log log_line_v4_val4_id_03
