9c270850d6e62769acc7d42e4cb9e70d164eb699
pingaowang <pingaowang2015@gmail.com>
2019-12-22 00:30:48 +0800

    exp: negative loss nz

---
+++ train_line_v4_val4_id-02.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- codinbg: utf-8 -*-
+
+python3 train.py -l 0.001 --resize 500 --nz -0.0001 -d 0.99 -e 1000 -b 4 -g --task-type line --data data/dataset_line_v4_val4 --log log_line_v4_val4_id_02_negative_nz
